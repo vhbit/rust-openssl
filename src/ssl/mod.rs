f4fc0dc7461491c5ea083bf4aa3a4f318e9fdc5f
Valerii Hiora <valerii.hiora@gmail.com>
2014-09-12 20:37:23 +0300

    Enabling TLS1.2 support
    Unfortunately OS X comes with 0.9.8 bundled. There is a way to
    install a recent version through homebrew, however it is
    extremely hard to make it link agains brewed version without
    tricking link version
    
    Conflicts:
    	src/ssl/ffi.rs

--- src/ssl/mod.rs
+++ src/ssl/mod.rs
@@ -49,6 +49,7 @@ fn init() {
 
 /// Determines the SSL method supported
 #[deriving(Show, Hash, PartialEq, Eq)]
+#[allow(non_camel_case_types)]
 pub enum SslMethod {
     #[cfg(sslv2)]
     /// Only support the SSLv2 protocol
@@ -59,6 +60,8 @@ pub enum SslMethod {
     Tlsv1,
     /// Support the SSLv2, SSLv3 and TLSv1 protocols
     Sslv23,
+    Tlsv1_1,
+    Tlsv1_2,
 }
 
 impl SslMethod {
@@ -68,7 +71,9 @@ impl SslMethod {
             Sslv2 => ffi::SSLv2_method(),
             Sslv3 => ffi::SSLv3_method(),
             Tlsv1 => ffi::TLSv1_method(),
-            Sslv23 => ffi::SSLv23_method()
+            Sslv23 => ffi::SSLv23_method(),
+            Tlsv1_1 => ffi::TLSv1_1_method(),
+            Tlsv1_2 => ffi::TLSv1_2_method()
         }
     }
 }
